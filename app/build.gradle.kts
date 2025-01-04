6d0fe684d9dacd13376d3c3f7c1e9c5201b4c078
Ngoc Tan <ngoctan9102000@gmail.com>
2025-01-04 21:44:32 +0700

    add libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.com.google.devtools.ksp)
 }
 
 android {
@@ -26,6 +27,11 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -45,4 +51,20 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+//    implementation("com.google.code.gson:gson:2.11.0")
+//    implementation("androidx.room:room-runtime:2.6.1")
+//    ksp("androidx.room:room-compiler:2.6.1")
+//    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+//    implementation 'com.squareup.retrofit2:retrofit:(2.11.0)'
+//    implementation 'com.squareup.retrofit2:converter-gson:2.11.0'
+
+    implementation(libs.converter.gson)
+    implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.gson)
+
 }
